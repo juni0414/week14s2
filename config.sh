428d046b3bdbb22c3a6481e8e6def3ff637f3bca
juni0414 <l1f21bsse0414@ucp.edu.pk>
2025-01-21 11:48:48 +0500

    intial commit

---
+++ config.sh
@@ -0,0 +1,7 @@
+echo "going to configure repo"
+git branch -m develop
+git config --local user.name juni0414
+git config --local user.email l1f21bsse0414@ucp.edu.pk
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./src/index.html ./styles/index.css .gitignore
